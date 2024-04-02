5b66ad916324b2205efa2de61feeb7feab853c85
Stănilă Ioan <ioanstanilais@gmail.com>
2024-04-02 13:51:37 +0300

    move examples in the cesrinf crate

--- web/cesrinf-web-docker/src/routes.rs
+++ web/cesrinf-web-docker/src/routes.rs
@@ -3,28 +3,8 @@ use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::Json;
 use cesrinf::domain::ParsedData;
-use cesrinf::{CesrParser, CesrinfError};
+use cesrinf::{CesrParser, CesrinfError, EXAMPLES};
 use serde::Serialize;
-
-const EXAMPLES: &[&str] = &[
-    "AJ97qKeoQzmWJvqxmeuqIMQbRxHErlNBUsm9BJ2FKX6T",
-    "AKrRTkeoyVnRBOJG03ZXeSroKhM7j_YyNurHuWxf8wgzAKrRTkeoyVnRBOJG03ZXeSroKhM7j_YyNurHuWxf8wgz",
-    "0BAcbTzvOldV43AGAW6yv1luylr4BPb06B8PiycQ1SbON09QPQW3812nzlzaitf-hmyCSG-mevc3_kOHDT3pj6AA",
-    "1AAJA3cK_P2CDlh-_EMFPvyqTPI1POkw-dr14DANx5JEXDCZ",
-    "-AAD\
-        AA5267UlFg1jHee4Dauht77SzGl8WUC_0oimYG5If3SdIOSzWM8Qs9SFajAilQcozXJVnbkY5stG_K4NbKdNB4AQ\
-        ABBgeqntZW3Gu4HL0h3odYz6LaZ_SMfmITL-Btoq_7OZFe3L16jmOe49Ur108wH7mnBaq2E_0U0N0c5vgrJtDpAQ\
-        ACTD7NDX93ZGTkZBBuSeSGsAQ7u0hngpNTZTK_Um7rUZGnLRNJvo5oOnnC1J2iBQHuxoq8PyjdT3BHS2LiPrs2Cg",
-    "-FAB\
-        E_T2_p83_gRSuAYvGhqV3S0JzYEF2dIa-OCPLbIhBO7Y\
-        -EAB\
-        0AAAAAAAAAAAAAAAAAAAAAAB\
-        EwmQtlcszNoEIDfqD-Zih3N6o5B3humRKvBBln2juTEM\
-        -AAD\
-        AA5267UlFg1jHee4Dauht77SzGl8WUC_0oimYG5If3SdIOSzWM8Qs9SFajAilQcozXJVnbkY5stG_K4NbKdNB4AQ\
-        ABBgeqntZW3Gu4HL0h3odYz6LaZ_SMfmITL-Btoq_7OZFe3L16jmOe49Ur108wH7mnBaq2E_0U0N0c5vgrJtDpAQ\
-        ACTD7NDX93ZGTkZBBuSeSGsAQ7u0hngpNTZTK_Um7rUZGnLRNJvo5oOnnC1J2iBQHuxoq8PyjdT3BHS2LiPrs2Cg",
-];
 
 #[derive(Template)]
 #[template(path = "index.html")]
